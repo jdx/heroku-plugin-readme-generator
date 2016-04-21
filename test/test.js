6fa10802b9a9476824988f1d5cd0095696426c79
Jeff Dickey <dickeyxxx@gmail.com>
2016-04-21 13:05:52 -0700

    setting up to be a real project

---
+++ test/test.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+'use strict'
+/* global describe it */
+
+let expect = require('unexpected')
+
+describe('generation', () => {
+  it('generates a readme', () => {
+    expect(1, 'to be', 1)
+  })
+})
